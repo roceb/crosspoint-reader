8f3d226bf3af88fad24ed9c0e7564638d44c5c77
Jake Kenneally <jkenneally@tekmetric.com>
2026-01-20 10:27:55 -0600

    increment versions to prevent error when opening cached EPUBs

--- lib/Epub/Epub/Section.cpp
+++ lib/Epub/Epub/Section.cpp
@@ -8,7 +8,7 @@
 #include "parsers/ChapterHtmlSlimParser.h"
 
 namespace {
-constexpr uint8_t SECTION_FILE_VERSION = 10;
+constexpr uint8_t SECTION_FILE_VERSION = 11;
 constexpr uint32_t HEADER_SIZE = sizeof(uint8_t) + sizeof(int) + sizeof(float) + sizeof(bool) + sizeof(uint8_t) +
                                  sizeof(uint16_t) + sizeof(uint16_t) + sizeof(uint16_t) + sizeof(bool) +
                                  sizeof(uint32_t);
